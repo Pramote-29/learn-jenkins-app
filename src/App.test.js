bf186b9cad72f30dabe1cac7a504c8326c712d5e
Pramote Numiam <manaziza001@gmail.com>
2025-04-03 18:35:43 +0700

    fixed some issue

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,22 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn Jenkins link', () => {
+test('renders DevOps project text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn Jenkins/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText(/this is my final DevOps project/i);
+  expect(titleElement).toBeInTheDocument();
+});
+
+test('renders version text', () => {
+  render(<App />);
+  const versionElement = screen.getByText(/Application version/i);
+  expect(versionElement).toBeInTheDocument();
+});
+
+test('renders the Jenkins logo container', () => {
+  render(<App />);
+  // ค้นหาจาก test ID แทนการใช้ role
+  const { container } = render(<App />);
+  const logoContainer = container.querySelector('.logo-container');
+  expect(logoContainer).toBeInTheDocument();
 });
